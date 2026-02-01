a8d4bdf6f85f730bff7a2f034a38daafea757862
aliaskarbegzak7 <aliaskarbegzak7@gmail.com>
2026-02-01 21:24:40 +0500

    hellooo0

---
+++ prac02/loops/for.py
@@ -0,0 +1,51 @@
+#For Loops
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+# apple banana cherry
+
+for x in "banana":
+  print(x)
+# b a n a n a
+
+for x in range(6):
+  print(x)
+# 0 1 2 3 4 5
+
+for x in range(2, 6):
+  print(x)
+# 2 3 4 5
+
+for x in range(2, 30, 3):
+  print(x)
+# 2 5 8 11 14 17 20 23 26 29
+
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+# 0 1 2 3 4 Finally finished!
+
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+# 0 1 2
+
+
+#For Loops Break
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+# apple
+
+#For Loops Continue
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+# apple cherry
