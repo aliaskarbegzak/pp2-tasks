a3abc04f6e6657304ac5a3e17b1c265edc9eff2a
aliaskarbegzak <aliaskarbegzak7@gmail.com>
2026-03-06 18:32:05 +0500

    Prac1-2-3

---
+++ prac02/loops/while.py
@@ -0,0 +1,34 @@
+#While Loops
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+# 1 2 3 4 5
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+# 1 2 3 4 5 i is no longer less than 6
+
+
+#While Loops Break
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+# 1 2 3
+
+
+#While Loops Continue
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+# 1 2 4 5 6
