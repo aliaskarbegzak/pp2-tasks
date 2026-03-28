6f096a8a6e9ca6b7b1ed7f4ddf3a09bbec9f2a11
Aliaskar Begzak <a_begzak@kbtu.kz>
2026-03-28 08:52:11 +0500

    prac07

---
+++ prac07/config.py
@@ -0,0 +1,12 @@
+def load_config():
+    return {
+        "host": "localhost",
+        "database": "basement",
+        "user": "postgres",
+        "password": "povarzxcvbnm1234"
+    }
+
+
+
+
+
